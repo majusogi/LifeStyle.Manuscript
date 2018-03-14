018594ceca43b2eaaea56c9685fbbfc088867e7d
Juliana Soto-Giron <dendroapsis@gmail.com>
2018-03-14 15:36:39 -0400

    Update ParsingMGs.QualityCheck.bash

--- Scripts/ParsingMGs.QualityCheck.bash
+++ Scripts/ParsingMGs.QualityCheck.bash
@@ -54,20 +54,90 @@ module load zlib/1.2.8
 module load boost/1.57.0
 module load mash/1.0.2
 
-sketching first
+###sketching first
+Sketch size corresponds to the number of (non-redundant) min-hashes that are kept.
+For sequences to be compared with mash, they must first be sketched, which creates vastly reduced representations of them. 
+This will happen automatically if mash dist is given raw sequences. However, if multiple comparisons will be performed, 
+it is more efficient to create sketches with mash sketch first and provide them to mash dist in place of the raw sequences. 
+mash sketch creates a sketch file, which is a reduced representation of a sequence or set of sequences (based on min-hashes) that can be used for fast distance estimations. 
+Input can be fasta or fastq files: Coupled.Reads.fna
 
-For sequences to be compared with mash, they must first be sketched, which creates vastly reduced representations of them. This will happen automatically if mash dist is given raw sequences. However, if multiple comparisons will be performed, it is more efficient to create sketches with mash sketch first and provide them to mash dist in place of the raw sequences. mash sketch creates a sketch file, which is a reduced representation of a sequence or set of sequences (based on min-hashes) that can be used for fast distance estimations. Input can be fasta or fastq files.
-
-mash sketch -l list.txt -o ~/data3/from-pmicro1/norovirus/metagenomes/12.MASH -k 25 -s 10000
+Folder: 03.MASH.dist
+for i in /nv/hp10/mjsg3/shared3/projects/EcoZUR/MGs.lifestyle/Rios.controls/data/04.trimmed_fasta/*.CoupledReads.fa; 
+do mash sketch -k 25 -s 10000 $i -o $(basename $i .CoupledReads.fa); done
 
 options
+-l List input. Each file contains a list of sequence files, one per line.
+-k Kmer size. Hashes will be based on strings of this many nucleotides 
+-s Sketch size. Each sketch will have at most this many non-redundant min-hashes (Default: 1000)
+
+
+###Calculating distances:
+
+mash dist <query1> <query2>
+
+To automate this analysis, I used a bash script ‘dist.mash’. 
+
+#!/bin/bash
+
+CTG=$(ls *.msh)
+
+for i in $CTG ; do
+   for j in $CTG ; do
+      [[ "$i" == "$j" ]] && break
+      o="dist/$(basename $i .msh)-$(basename $j .msh)"
+      [[ -s $o.dist ]] || mash dist  $i  $j   > $o.dist
+   done
+done
+
+
+Given k-mer sets A and B, the MinHash algorithm provides an estimation of the Jaccard index:
+The Jaccard index is a useful measure of global sequence similarity because it correlates with ANI.
 
 
 
-8. MetaPhlAn: Metagenomic Phylogenetic Analysis (MetaPhlAn version 1.7.7 )
+
+
+
+6. MetaPhlAn: Metagenomic Phylogenetic Analysis (MetaPhlAn version 1.7.7 )
 
 module load python/2.7
 module load bowtie2/2.1.0
+
+./metaphlan2.py 4-4M.CoupledReads.fa --input_type fasta  --bowtie2out metagenome.bowtie2.bz2  --nproc 16 > profiled_metagenome.txt
+
+./metaphlan2.py --input_type fasta environmental.CoupledReads.fa --tax_lev f --bowtie2out envri.bowtie2.bz2 --nproc 16 > profiled_envi.MG.txt
+
+
+
+to classify at different levels using the envri.bowtie2.bz2 already computed:
+
+./metaphlan2.py --input_type bowtie2out Cas27.bt2out  --tax_lev f > pfile.family.cas27.txt
+
+
+
+for Multiple samples: 
+
+for f in */*.bowtie2.bz2; do /nv/hp10/mjsg3/data/tools/metaphlan2/metaphlan2.py $f --input_type bowtie2out --tax_lev g --nproc 16 > ${f%.bowtie2.bz2}_prof.genus.txt; done
+
+
+
+
+
+2. merge metaphaln tables. The resulting table contains relative abundances with microbial clades as rows and samples as columns.
+
+utils/merge_metaphlan_tables.py /09.MetaPhlAn/profiled_samples/*.txt > 09.MetaPhlAn/output/merged_abundance_table.txt
+
+/nv/hp10/mjsg3/data/tools/metaphlan2/utils/merge_metaphlan_tables.py casP7/profiled_casP7.txt casP4/profiled_casP4.txt casP5/profiled_casP5.txt casP8/profiled_casP8.txt > merged_abundance_table.txt
+
+
+
+Visualization with Krona: 
+
+./metaphlan2krona.py -p ../profiled_MG.27.txt -k krona.27.txt
+
+ /Users/mjsg3/Software/KronaTools-2.4/scripts/ImportText.pl  LC1.krona.txt -o krona.27.html
+
 
 
 
